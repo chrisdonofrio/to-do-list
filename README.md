92163d8c6e78e46124329211c395a9937fe3479d
Chris Donofrio <cdonofrio20@gmail.com>
2015-11-23 13:08:53 -0500

    Completed to-do list ver. 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # to-do-list
+
+This is a very simple task manager / to-do-list. Some jQuery practice here.
+
+Want to add extra functionality at a later date (For instance, return an error so the form cannot accept duplicate tasks).
--- js/dom.js
+++ js/dom.js
@@ -1,20 +1,44 @@
 $(document).ready(function() {
 
+// Event listener for submitting new task via the 'Enter' key
+
+    $('#newTask').keydown(function(event) {
+        if (event.keyCode == 13) {
+            $('.btn-success').trigger('click');
+        }
+    });
+
+// Event listener for submitting new task via 'Submit' button click
+
+    $(".btn-success").on("click", function(e) {
+        e.preventDefault();
+
+        var taskInput = $("#newTask").val();
+
+// Exit loop if blank entry is submitted
+
+        if (taskInput === "") {
+            return;
+        }
+
+// Appending new list items to columns
+
+        var addListItem = '<li id="listItemAdd"><button class="btn btn-primary checked"><span class="glyphicon glyphicon-ok">' + '<button class="btn btn-danger remove"><span class="glyphicon glyphicon-remove"></span></button><p>' + taskInput + '</p></li>'
+        $(".list-unstyled").append(addListItem);
+        $(".btn-primary").on("click", function(e) {
+            $(this).parent().addClass("strikethrough");
+            // $(this).children().addClass("clicked");
+
+        });
+
+// Removing task from column
+
+        $(".btn-danger").on("click", function(e) {
+            $(this).parent().remove();
+        });
+
+    });
+
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	});
